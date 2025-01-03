b2190e6c481afc172a9335355460025b2b261540
TLiiv <110547191+TLiiv@users.noreply.github.com>
2025-01-03 20:10:11 +0200

    minor bug fix

--- RealEstate.Server/obj/Debug/net8.0/RealEstate.Server.AssemblyInfo.cs
+++ RealEstate.Server/obj/Debug/net8.0/RealEstate.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c3343df814ca72b3b17220e44bcd93c04db3d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3827e6126feb61b7747253729ee394d4eaae017")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- realestate.client/src/App.tsx
+++ realestate.client/src/App.tsx
@@ -1,58 +1,113 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
+//interface Forecast {
+//    date: string;
+//    temperatureC: number;
+//    temperatureF: number;
+//    summary: string;
+//}
+
+interface Users {
+    UserFirstName: string;
 }
 
 function App() {
-    const [forecasts, setForecasts] = useState<Forecast[]>();
+    const [users, setUsers] = useState<Users[]>([]);
+
+    async function populateUsersData() {
+        const response = await fetch('users');
+        if (response.ok) {
+            const data = await response.json();
+            setUsers(data);
+        }
+    }
 
     useEffect(() => {
-        populateWeatherData();
+        populateUsersData();
     }, []);
 
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tableLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
+    const contents = users.length > 0 ? (
+        <ul>
+            {users.map((u, index) => (
+                <li key={index}>{u.UserFirstName}</li>
+            ))}
+        </ul>
+    ) : (
+        <p>Loading users...</p>
+    );
+
+    
 
     return (
         <div>
-            <h1 id="tableLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
             {contents}
         </div>
     );
 
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        if (response.ok) {
-            const data = await response.json();
-            setForecasts(data);
-        }
-    }
+   
 }
 
 export default App;
+
+
+//import { useEffect, useState } from 'react';
+//import './App.css';
+
+//interface Forecast {
+//    date: string;
+//    temperatureC: number;
+//    temperatureF: number;
+//    summary: string;
+//}
+
+//function App() {
+//    const [forecasts, setForecasts] = useState<Forecast[]>();
+
+//    useEffect(() => {
+//        populateWeatherData();
+//    }, []);
+
+    
+
+//    const contents = forecasts === undefined
+//        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
+//        : <table className="table table-striped" aria-labelledby="tableLabel">
+//            <thead>
+//                <tr>
+//                    <th>Date</th>
+//                    <th>Temp. (C)</th>
+//                    <th>Temp. (F)</th>
+//                    <th>Summary</th>
+//                </tr>
+//            </thead>
+//            <tbody>
+//                {forecasts.map(forecast =>
+//                    <tr key={forecast.date}>
+//                        <td>{forecast.date}</td>
+//                        <td>{forecast.temperatureC}</td>
+//                        <td>{forecast.temperatureF}</td>
+//                        <td>{forecast.summary}</td>
+//                    </tr>
+//                )}
+//            </tbody>
+//        </table>;
+
+//    return (
+//        <div>
+//            <h1 id="tableLabel">Weather forecast</h1>
+//            <p>This component demonstrates fetching data from the server.</p>
+//            {contents}
+//        </div>
+//    );
+
+//    async function populateWeatherData() {
+//        const response = await fetch('weatherforecast');
+//        if (response.ok) {
+//            const data = await response.json();
+//            setForecasts(data);
+//        }
+//    }
+//}
+
+//export default App;
